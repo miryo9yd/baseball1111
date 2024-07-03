d5feeb4347fa2c3f65706abee1021d0ddb9205f4
[youngdae.seo] <miryo9yd@naver.com>
2024-07-03 15:07:38 +0900

    [refactoring] 31 Page

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -9,5 +9,12 @@ TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
 
 TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
 	Baseball game;
-	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+	try {
+		game.guess(string("12s"));
+		FAIL(); 
+	}catch(exception e) {
+
+	}
+
+	
 }
