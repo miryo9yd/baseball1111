4d75d8e1d882d6138b8127904e9abae8db311b05
[youngdae.seo] <miryo9yd@naver.com>
2024-07-03 15:02:23 +0900

    [feautre] 27 Page

---
+++ BaseBall/baseball.cpp
@@ -0,0 +1,8 @@
+#include <stdexcept>
+using namespace std;
+class Baseball {
+public : 
+	void guess(const string& string) {
+		throw length_error("Must be three letters.");
+	}
+};
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,8 @@
 #include "pch.h"
 #include "../BaseBall/baseball.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
+	Baseball game; 
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
