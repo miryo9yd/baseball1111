bf4c6a7c3a9acaf7f62c1e03c4b44dd7ab7fb700
[youngdae.seo] <miryo9yd@naver.com>
2024-07-03 15:03:35 +0900

    [refactoring] 28 Page

--- BaseBall/baseball.cpp
+++ BaseBall/baseball.cpp
@@ -3,6 +3,10 @@ using namespace std;
 class Baseball {
 public : 
 	void guess(const string& string) {
-		throw length_error("Must be three letters.");
+		if (string.length() != 3) {
+
+			throw length_error("Must be three letters.");
+		}
+			
 	}
 };
