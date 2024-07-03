176b1774b029ad7dfb9bf9e1d12e7653de666db5
[youngdae.seo] <miryo9yd@naver.com>
2024-07-03 15:42:55 +0900

    [feautre] 49 Page

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -3,7 +3,7 @@
 
 class BaseballFixture : public testing::Test {
 public : 
-	Baseball game;
+	Baseball game{ "123" };
 	void assertIllegalArgument(string guessNumber) {
 		// game.guess() 수행후 Exception이 발생해야 PASS !
 		try {
@@ -22,4 +22,13 @@ TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
 	assertIllegalArgument("12");
 	assertIllegalArgument("12s");
 	assertIllegalArgument("121");
+}
+
+TEST_F(BaseballFixture, ReturnSolveResultIfMatchedNumber) {
+
+	GuessResult result = game.guess("123");
+
+	EXPECT_TRUE(result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
