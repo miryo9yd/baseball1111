51d17fd38fb7d133ab23b61e20ee38e3eae6129a
[youngdae.seo] <miryo9yd@naver.com>
2024-07-03 15:11:01 +0900

    [refactoring] 35 Page

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,20 +1,27 @@
 #include "pch.h"
 #include "../BaseBall/baseball.cpp"
 
+class BaseballFixture : public testing::Test {
+public : 
+	Baseball game;
+	void assertIllegalArgument(string guessNumber) {
+		// game.guess() 수행후 Exception이 발생해야 PASS !
+		try {
+			game.guess(guessNumber);
+			FAIL();
+		}
+		catch (exception e) {
 
-TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
-	Baseball game; 
-	EXPECT_THROW(game.guess(string("12")), length_error);
+		}
+	}
+};
+
+TEST_F(BaseballFixture, ThrowExceptionWhenInputLengthIsUnmached) {
+	assertIllegalArgument("12");
 }
 
-TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
-	Baseball game;
-	try {
-		game.guess(string("12s"));
-		FAIL(); 
-	}catch(exception e) {
-
-	}
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidChar) {
+	assertIllegalArgument("12s");
 
 	
 }
